37f53092b883908addc37c163650dbcf98781f5d
wander wildwood <wanderwildwood@users.noreply.github.com>
2026-09-25 20:36:28 -0400

    0.3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
 
     defaultConfig {
         applicationId = "com.wanderwildwood.kirinuki"
-        versionCode = 16
-        versionName = "0.3.8"
+        versionCode = 17
+        versionName = "0.3.9"
         // TLS1.3 is enabled in Android 10 (29) and above
         minSdk = 29
         targetSdk =
